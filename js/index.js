d313332a559aa118eb2176f4f94e0e93dfde0b0b
Artem Balanovskyi <mr.balanovsky@gmail.com>
2023-04-27 13:58:52 +0300

    feat: add result page countdown and links movement

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@
 }());
 
 // Scroll to anchors
+
 (function () {
 
 	const smoothScroll = function (targetEl, duration) {
@@ -57,3 +58,60 @@
 	};
 	scrollTo();
 }());
+
+// Results page timer
+
+function countdown( elementName, minutes, seconds )
+{
+	let element, endTime, hours, mins, msLeft, time;
+	
+	function twoDigits( n )
+	{
+		return (n <= 9 ? "0" + n : n);
+	}
+	
+	function updateTimer()
+	{
+		msLeft = endTime - (+new Date);
+		if ( msLeft < 1000 ) {
+			element.innerHTML = "Time is up!";
+		} else {
+			time = new Date( msLeft );
+			hours = time.getUTCHours();
+			mins = time.getUTCMinutes();
+			element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+			setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+		}
+	}
+	
+	element = document.getElementById( elementName );
+	endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+	updateTimer();
+}
+
+// countdown( "results__timer", 10, 0 );
+
+// Pass test button
+
+const mainPage = document.querySelector('.main-page')
+const testPage = document.querySelector('.tests-page')
+const headerBasement = document.querySelector('.header__basement')
+let buttonsArray = document.querySelectorAll(".pass-test-button");
+
+buttonsArray.forEach(function(button) {
+	button.addEventListener("click", showTestsPage);
+});
+
+
+function showMainPage () {
+	console.log('click!')
+	testPage.classList.add('hidden')
+	mainPage.classList.remove('hidden')
+	headerBasement.style.height = '46'
+}
+
+function showTestsPage () {
+	mainPage.classList.add('hidden')
+	headerBasement.style.height = '0'
+	testPage.classList.remove('hidden')
+}
