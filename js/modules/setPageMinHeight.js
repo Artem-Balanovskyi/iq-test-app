94bec9994fbb8158f1879619f1b46f9465b07985
Artem Balanovskyi <mr.balanovsky@gmail.com>
2023-05-02 16:35:37 +0300

    fix: fix page height on different devices

--- js/modules/setPageMinHeight.js
+++ js/modules/setPageMinHeight.js
@@ -5,14 +5,9 @@ const mainBlock = document.querySelector('.main-block')
 let footerBottomStyle = 0
 
 export function setPageMinHeight() {
-	testsWrapper.style.minHeight = `${screen.height}px`
-	console.log(`screenHeight: ${screen.height} px`)
-	console.log(`screenWidth: ${screen.width} px`)
-	console.log(`window.innerHeight: ${window.innerHeight} px`)
-	console.log(`window.innerWidth: ${window.innerWidth} px`)
-	console.log(`testsWrapper: ${testsWrapper.style.minHeight}`)
-	// mainBlock.style.maxHeight = `${1928}px`;
+	testsWrapper.style.minHeight = `${window.innerHeight}px`
+	mainBlock.style.maxHeight = `${1928}px`;
 	if (screen.height >= 639) {
-		results.style.minHeight = `${screen.height + 21}px`
+		results.style.minHeight = `${window.innerHeight + 21}px`
 	}
 }
